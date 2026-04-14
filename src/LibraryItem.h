e84ed698101766a9e4e9d318a7b2cdad7828950f
pshukla3 <24shuklaparth@gmail.com>
2026-04-14 14:41:02 -0700

    Implement LibraryItem class with title and id
    Added LibraryItem class with basic properties and methods.

--- src/LibraryItem.h
+++ src/LibraryItem.h
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+class LibraryItem
+{
+protected:
+    string title;
+    int id;
+    bool isAvaliable;
+
+public:
+    LibraryItem(string title, int id);
+    virtual ~LibraryItem();
+
+    virtual void displayInfo() const = 0;
+
+    string getTitle();
+    int getId();
+};
