0032c9b723c46adb2dc1ac445570f2cfbc9e905e
pshukla3 <24shuklaparth@gmail.com>
2026-04-18 14:02:06 -0700

    Implement LibraryManager methods for item management

--- src/LibraryManager.cpp
+++ src/LibraryManager.cpp
@@ -1,1 +1,65 @@
+#include <iostream>
+#include "LibraryManager.h"
+#include "Borrowable.h"
+#include "Exceptions.h"
 
+using namespace std;
+
+LibraryManager::LibraryManager() {
+}
+
+void LibraryManager::addItem(LibraryItem* item) {
+    items.push_back(item);
+}
+
+void LibraryManager::removeItem(int id) {
+    for (int i = 0; i < items.size(); i++) {
+        if (items[i]->getId() == id) {
+            delete items[i]; 
+            items.erase(items.begin() + i);
+            return;
+        }
+    }
+    throw ItemNotFoundException();
+}
+
+LibraryItem* LibraryManager::findItem(int id) {
+    for (int i = 0; i < items.size(); i++) {
+        if (items[i]->getId() == id) {
+            return items[i];
+        }
+    }
+    throw ItemNotFoundException();
+}
+
+void LibraryManager::displayAll() const {
+    cout << "\n--- Library Catalog ---" << endl;
+    for (int i = 0; i < items.size(); i++) {
+        items[i]->displayInfo();
+    }
+    cout << "-----------------------" << endl;
+}
+
+void LibraryManager::borrowItem(int id) {
+    LibraryItem* item = findItem(id); 
+    
+    Borrowable* bItem = dynamic_cast<Borrowable*>(item);
+    
+    if (bItem != nullptr) {
+        bItem->borrowItem();
+    } else {
+        throw InvalidOperationException();
+    }
+}
+
+void LibraryManager::returnItem(int id) {
+    LibraryItem* item = findItem(id);
+    
+    Borrowable* bItem = dynamic_cast<Borrowable*>(item);
+    
+    if (bItem != nullptr) {
+        bItem->returnItem();
+    } else {
+        throw InvalidOperationException();
+    }
+}
