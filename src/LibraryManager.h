94095cbb47727d0db9311b875824e216c470d547
pshukla3 <24shuklaparth@gmail.com>
2026-04-18 14:00:17 -0700

    Add LibraryManager class definition

--- src/LibraryManager.h
+++ src/LibraryManager.h
@@ -1,1 +1,22 @@
+#ifndef LIBRARYMANAGER_H
+#define LIBRARYMANAGER_H
+#include <vector>
+#include "LibraryItem.h"
 
+using namespace std; 
+
+class LibraryManager{
+private: 
+    vector<LibraryItem*> items;
+public: 
+    LibraryManager()
+    void addItem(LibraryItem* item);
+    void removeItem(int id);
+    LibraryItem* findItem(int id);
+    void displayAll() const;
+    
+    void borrowItem(int id);
+    void returnItem(int id);
+};
+
+#endif
