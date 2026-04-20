cef853c1e551ae91ec122ebda0632f256b15b9fb
pshukla3 <24shuklaparth@gmail.com>
2026-04-20 10:38:58 -0700

    Refactor main to use myLib for library operations

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,54 @@
-
 #include <iostream>
 #include "LibraryManager.h"
 #include "Book.h"
 #include "Audiobook.h"
 #include "Magazine.h"
 #include "Exceptions.h"
+
 using namespace std;
 
 int main() {
-    LibraryManager manager;
+    LibraryManager myLib;
 
-    manager.addItem(new Book("1984", 1, "Orwell", 328));
-    manager.addItem(new Audiobook("Dune", 2, "Simon Vance", 21.5));
-    manager.addItem(new Magazine("Time", 3, 101, "Time Inc."));
+    cout << "Adding items to the library..." << endl;
+    myLib.addItem(new Book("The Hobbit", 10, "J.R.R. Tolkien", 310));
+    myLib.addItem(new Audiobook("Atomic Habits", 11, "James Clear", 5.5));
+    myLib.addItem(new Magazine("Wired", 12, 45, "Conde Nast"));
 
-    manager.displayAll();
+    myLib.displayAll();
 
+    cout << "\nTesting checkout system..." << endl;
     try {
-        manager.borrowItem(1);
-
-        manager.borrowItem(1);
-    }
+        myLib.borrowItem(10);
+        cout << "Checked out item 10 successfully." << endl;
+        
+        cout << "Trying to check it out again: ";
+        myLib.borrowItem(10); 
+    } 
     catch (exception& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << e.what() << endl;
     }
 
+    cout << "\nTesting interface restrictions..." << endl;
     try {
-        manager.borrowItem(3);
-    }
+        cout << "Trying to borrow a magazine: ";
+        myLib.borrowItem(12);
+    } 
     catch (exception& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << e.what() << endl;
     }
+
+    cout << "\nTesting invalid ID..." << endl;
+    try {
+        cout << "Looking for item 500: ";
+        myLib.borrowItem(500);
+    } 
+    catch (exception& e) {
+        cout << e.what() << endl;
+    }
+
+    cout << "\nFinal status check:" << endl;
+    myLib.displayAll();
 
     return 0;
 }
